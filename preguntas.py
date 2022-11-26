2b20deb6c239550426125e50bccf37e3635176c9
Juan José Alzate Garcés <64942729+jjalgarces@users.noreply.github.com>
2022-11-26 17:00:45 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -153,17 +153,19 @@ def pregunta_05():
     estimator.fit(x_train, y_train)  #
 
     # Pronostique para las muestras de entrenamiento y validacion
-    y_trian_pred = estimator.predict(x_train)
-    y_test_pred = estimator.predict(x_test) 
+    y_train_pred = estimator.predict(x_train)
+    y_test_pred = estimator.predict(x_test)
 
     # Calcule el error cuadrático medio de las muestras
     mse_train = mean_squared_error(  
-        y_train,  
-        y_trian_pred,  
+        y_train,
+        y_train_pred,
+
     )
     mse_test = mean_squared_error(  
-        y_test,  
-        y_test_pred,  
+        y_test,
+        y_test_pred,
+
     )
 
     # Retorne el mse de entrenamiento y prueba
